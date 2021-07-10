268dd314170415b1686ded420842054563a77088
wjchahaha <1678912421@qq.com>
2021-07-10 20:45:08 +0800

    1
    项目初始化结构

---
+++ gulimall-coupon/src/test/java/com/jc/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
