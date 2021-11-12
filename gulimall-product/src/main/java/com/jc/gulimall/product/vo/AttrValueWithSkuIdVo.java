31c4879437860530271405b4e86ab7952f94c96d
wjchahaha <1678912421@qq.com>
2021-11-12 22:28:27 +0800

    销售属性渲染
    通过当前sku详情页回显销售属性值（将销售属性值设置为红框框）

---
+++ gulimall-product/src/main/java/com/jc/gulimall/product/vo/AttrValueWithSkuIdVo.java
@@ -0,0 +1,18 @@
+package com.jc.gulimall.product.vo;
+
+import lombok.Data;
+
+/**
+* @program: gulimall
+*
+* @description: 
+*
+* @author: Mr.Wang
+*
+* @create: 2021-11-12 20:33
+**/
+@Data
+public class AttrValueWithSkuIdVo {
+    private String attrValue;
+    private String skuIds;
+}
